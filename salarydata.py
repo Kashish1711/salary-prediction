e3b6ad6474556fc8bdf6f6bab7921bcfa7cdd539
Kashish1711 <kashish1275.be24@chitkarauniversity.edu.in>
2026-05-18 13:04:14 +0530

    Updated salarydata.py to improve structure

--- salarydata.py
+++ salarydata.py
@@ -1,50 +1,3 @@
-# # Import libraries
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# from sklearn.linear_model import LinearRegression
-# from sklearn.model_selection import train_test_split
-
-# # Load dataset
-# data = pd.read_csv("Salary_dataset.csv")
-
-# # Check missing values
-# print(data.isnull().sum())
-
-# # Handle missing values (fill with mean)
-# data['YearsExperience'].fillna(data['YearsExperience'].mean(), inplace=True)
-# data['Salary'].fillna(data['Salary'].mean(), inplace=True)
-
-# # Separate input and output
-# X = data[['YearsExperience']]
-# y = data['Salary']
-
-# # Train-test split (80% train, 20% test)
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# # Create model
-# model = LinearRegression()
-
-# # Train model
-# model.fit(X_train, y_train)
-
-# # Predict on test data
-# y_pred = model.predict(X_test)
-
-# # Print equation
-# print("Slope (m):", model.coef_[0])
-# print("Intercept (c):", model.intercept_)
-
-# # Plot graph (Test Data)
-# plt.scatter(X_test, y_test)
-# plt.plot(X_test, y_pred)
-# plt.xlabel("Experience")
-# plt.ylabel("Salary")
-# plt.title("Salary Prediction (Test Data)")
-# plt.show()
-
-
-
-
 # 1. Import libraries
 import pandas as pd
 import numpy as np
@@ -100,3 +53,49 @@ plt.title("Salary vs Experience (Test Set)")
 plt.xlabel("Years of Experience")
 plt.ylabel("Salary")
 plt.show()
+
+# # Import libraries
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split
+
+# # Load dataset
+# data = pd.read_csv("Salary_dataset.csv")
+
+# # Check missing values
+# print(data.isnull().sum())
+
+# # Handle missing values (fill with mean)
+# data['YearsExperience'].fillna(data['YearsExperience'].mean(), inplace=True)
+# data['Salary'].fillna(data['Salary'].mean(), inplace=True)
+
+# # Separate input and output
+# X = data[['YearsExperience']]
+# y = data['Salary']
+
+# # Train-test split (80% train, 20% test)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# # Create model
+# model = LinearRegression()
+
+# # Train model
+# model.fit(X_train, y_train)
+
+# # Predict on test data
+# y_pred = model.predict(X_test)
+
+# # Print equation
+# print("Slope (m):", model.coef_[0])
+# print("Intercept (c):", model.intercept_)
+
+# # Plot graph (Test Data)
+# plt.scatter(X_test, y_test)
+# plt.plot(X_test, y_pred)
+# plt.xlabel("Experience")
+# plt.ylabel("Salary")
+# plt.title("Salary Prediction (Test Data)")
+# plt.show()
+
+
